dffe38e86c0c6585653fe698f7fece6b5136789d
mre55 <misganawmihreteab>
2022-06-14 23:02:12 +0300

    Render modal component if any of the elemets are selected

--- ui/src/pages/posts/Post.jsx
+++ ui/src/pages/posts/Post.jsx
@@ -1,15 +1,25 @@
-const Post = ({ post }) => (
-  <div className="max-w-md w-full mx-auto py-4 border p-1.5">
-    <div className="h-96 w-full mb-2.5 flex items-center justify-center">
-      <img //
-        src={post.url}
-        alt={post.name}
-        className="max-w-full max-h-full border"
-      />
-    </div>
+import React, { useState } from 'react';
+import Modal from '../../components/Modal';
 
-    <p className="text-center text-lg font-semibold">{post.name}</p>
-  </div>
-);
+const Post = ({ post }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {isOpen ? (
+        <Modal post={post} setIsOpen={setIsOpen} />
+      ) : (
+        <div className="max-w-md w-full mx-auto py-4 border p-1.5" onClick={() => setIsOpen(true)}>
+          <div className="h-96 w-full mb-2.5 flex items-center justify-center">
+            {console.log('post from post compoent is ', post)}
+            <img src={post.url} alt={post.name} className="max-w-full max-h-full border" />
+          </div>
+
+          <p className="text-center text-lg font-semibold">{post.name}</p>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Post;
