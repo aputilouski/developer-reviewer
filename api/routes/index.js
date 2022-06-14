15b567836709ff72b5c927a25d4bd164db82f105
mre55 <misganawmihreteab>
2022-06-15 02:37:19 +0300

    Add pagination to the data coming from API

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,9 +5,32 @@ const { MemeModel } = require('../models');
 router.get('/', (req, res) => res.send('Hello from Express JS!'));
 
 router.get('/posts', async (req, res) => {
+
+  const page = parseInt(req.query.page || 1);
+  const limit = parseInt(req.query.limit || 8);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const rows = {};
+
+  if(startIndex > 0) {
+    rows.previous = {
+      page: page - 1,
+      limit: limit
+    }
+  }
+  
   try {
-    const rows = await MemeModel.findAll();
-    res.json({ rows });
+    const resultPosts = await MemeModel.findAll();
+    if(endIndex < resultPosts.length) {
+      rows.next = {
+        page: page + 1,
+        limit: limit
+      };
+    }
+    rows.rows = resultPosts.slice(startIndex, endIndex);
+    res.json( rows );
   } catch (error) {
     console.log(error);
     res.status(500).send(error.message || 'Some error occurred while fetching posts.');
