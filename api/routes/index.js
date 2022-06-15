83971f316643bf3672a7b2b793309b069c43c917
mre55 <misganawmihreteab>
2022-06-15 15:43:54 +0300

    Add start index and end index to fetch 8 recode at a time

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,26 +11,11 @@ router.get('/posts', async (req, res) => {
 
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
-
-  const rows = {};
-
-  if(startIndex > 0) {
-    rows.previous = {
-      page: page - 1,
-      limit: limit
-    }
-  }
   
   try {
     const resultPosts = await MemeModel.findAll();
-    if(endIndex < resultPosts.length) {
-      rows.next = {
-        page: page + 1,
-        limit: limit
-      };
-    }
-    rows.rows = resultPosts.slice(startIndex, endIndex);
-    res.json( rows );
+    const rows = resultPosts.slice(startIndex, endIndex);
+    res.json({ rows });
   } catch (error) {
     console.log(error);
     res.status(500).send(error.message || 'Some error occurred while fetching posts.');
