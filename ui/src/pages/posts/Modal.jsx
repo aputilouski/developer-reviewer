e539a227ce22fcc4395fa4bf53dcdb0efb7f75d9
mre55 <misganawmihreteab>
2022-06-15 16:41:43 +0300

    Add unique key when render post

--- ui/src/pages/posts/Modal.jsx
+++ ui/src/pages/posts/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ data, id }) => {
         <Spin spinning={!data?.length}>
           <div className="flex gap-3 overflow-x-auto">
             {data?.slice(data.findIndex(currentPost => currentPost.id === id)).map(post => (
-              <div className="w-[550px] mx-auto py-4 p-1.5">
+              <div key={post.id} className="w-[550px] mx-auto py-4 p-1.5">
                 <div className="h-96 mb-2.5 flex items-center justify-center">
                   <img src={post.url} alt={post.name} className="max-w-[530px] max-h-[260px] border" />
                 </div>
