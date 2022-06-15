a0898726ffa21317625510c6ce2f3e840b8a4842
mre55 <misganawmihreteab>
2022-06-15 16:41:17 +0300

    Bind modal to appElement for screen readers

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './AppRouter';
 import './index.css';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
