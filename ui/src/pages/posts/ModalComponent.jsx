b4accc7d444fd183f74d06c74bbe30149114b19f
mre55 <misganawmihreteab>
2022-06-15 19:11:02 +0300

    Increase image width

--- ui/src/pages/posts/ModalComponent.jsx
+++ ui/src/pages/posts/ModalComponent.jsx
@@ -38,7 +38,7 @@ const ModalComponent = ({ modalIsOpen, closeModal, data, id }) => {
                   {data?.slice(data.findIndex(currentPost => currentPost.id === id)).map(post => (
                     <div key={post.id} className="w-[550px] mx-auto py-4 p-1.5">
                       <div className="h-96 mb-2.5 flex items-center justify-center">
-                        <img src={post.url} alt={post.name} className="max-w-[530px] max-h-[260px] border" />
+                        <img src={post.url} alt={post.name} className="max-w-none w-[500px] max-h-[340px] border" />
                       </div>
 
                       <p className="text-center text-lg font-semibold">{post.name}</p>
