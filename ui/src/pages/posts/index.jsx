4c9617c9b704924d25ba10095a6e9c3f9229b6a6
mre55 <misganawmihreteab>
2022-06-15 18:59:30 +0300

    Restructure post index component

--- ui/src/pages/posts/index.jsx
+++ ui/src/pages/posts/index.jsx
@@ -2,27 +2,11 @@ import React, { useEffect, useState } from 'react';
 import api from 'api';
 import { Spin } from 'components';
 import useInfiniteScroll from './useInfinite';
-import Modal from 'react-modal';
-import ModalComponent from './Modal';
+import ModalComponent from './ModalComponent';
 
 const Posts = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-
-  const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      width: '80%',
-      height: '90%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-  };
 
   const loadData = () => {
     api.getPosts(page).then(res => {
@@ -47,7 +31,7 @@ const Posts = () => {
     setIsFetching(true);
   };
 
-  let subtitle;
+  
   useEffect(() => {
     loadData();
     window.addEventListener('scroll', isScrolling);
@@ -69,10 +53,6 @@ const Posts = () => {
     setIsOpen(true);
   };
 
-  const afterOpenModal = () => {
-    subtitle.style.color = '#f00';
-  };
-
   const closeModal = () => {
     setIsOpen(false);
   };
@@ -80,9 +60,9 @@ const Posts = () => {
   return (
     <>
       <Spin spinning={!data?.length}>
-        <div className="flex flex-col gap-3 relative">
+        <div className="flex flex-col gap-3">
           {data?.map(post => (
-            <div className="w-full mx-auto py-4 border p-1.5" onClick={() => openModal(post.id)}>
+            <div key={post.id} className="w-full mx-auto py-4 border p-1.5 hover:shadow-2xl" onClick={() => openModal(post.id)}>
               <div className="w-[1000px] h-96 mb-2.5 flex items-center justify-center">
                 <img src={post.url} alt={post.name} className="max-h-full border" />
               </div>
@@ -92,14 +72,7 @@ const Posts = () => {
           ))}
         </div>
       </Spin>
-      {modalIsOpen && (
-        <Spin spinning={!data?.length}>
-          <Modal isOpen={modalIsOpen} onAfterOpen={afterOpenModal} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
-            <button onClick={closeModal} className="flex justify-end">close</button>
-            <ModalComponent data={data} id={id} />
-          </Modal>
-        </Spin>
-      )}
+      {modalIsOpen && <ModalComponent modalIsOpen={modalIsOpen} closeModal={closeModal} data={data} id={id} />}
     </>
   );
 };
